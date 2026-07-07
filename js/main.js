ac3d42cd1055b0f9555f91c87988bc3eb439d104
Akash Chachere <88659038+akashchachere@users.noreply.github.com>
2026-07-07 10:06:09 +0530

    Add breakpoint for 3 slides per view at 1200px

--- js/main.js
+++ js/main.js
@@ -114,7 +114,7 @@ if (typeof Swiper !== 'undefined' && document.querySelector('.testimonials-swipe
     autoplay: { delay: 5000, disableOnInteraction: false },
     pagination: { el: '.swiper-pagination', clickable: true },
     navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    breakpoints: { 768: { slidesPerView: 2 } }
+    breakpoints: { 768: { slidesPerView: 2 }, 1200: { slidesPerView: 3 } }
   });
 }
 
